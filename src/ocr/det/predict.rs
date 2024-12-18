15cc4a0f723c3cea67883bf47234286d9b201e3a
tripg <phamtri2895@gmail.com>
2024-12-18 10:55:55 +0700

    init ort onnx runtime

--- src/ocr/det/predict.rs
+++ src/ocr/det/predict.rs
@@ -4,17 +4,45 @@ use ndarray::{Array1, Array3};
 use opencv::core::{copy_make_border, Mat, MatTraitConst, Scalar, Size, Vec3b, Vector, BORDER_CONSTANT};
 use opencv::imgcodecs::imwrite;
 use opencv::imgproc::{resize, INTER_LINEAR};
+use ort::execution_providers::CUDAExecutionProvider;
+use ort::session::builder::GraphOptimizationLevel;
+use ort::session::Session;
 
 pub struct TextDetector {
-    // model_path: String,
-    // bbox_threshold: f32,
-    // unclip_threshold: f32,
+    model_path: String,
+    bbox_threshold: f32,
+    unclip_ratio: f32,
 }
+
 
 impl TextDetector {
 
-    pub fn new() -> Self {
-        TextDetector {}
+    pub fn new(model_path: &str, bbox_threshold: Option<f32>, unclip_ratio: Option<f32>) -> Result<Self, Error> {
+        let bbox_threshold = bbox_threshold.unwrap_or(0.6);
+        let unclip_ratio= unclip_ratio.unwrap_or(1.6);
+
+        ort::init()
+            .with_execution_providers([CUDAExecutionProvider::default().build()])
+            .commit()?;
+
+        let model = Session::builder()?
+            .with_log_id(3.to_string())?
+            .with_optimization_level(GraphOptimizationLevel::Level3)?
+            .with_intra_threads(4)?
+            .with_execution_providers([CUDAExecutionProvider::default().build()])?
+            .commit_from_file(model_path)?;
+
+        Ok(TextDetector {
+            model_path: model_path.to_string(),
+            bbox_threshold,
+            unclip_ratio,
+        })
+    }
+
+    pub fn call(&self, bbox_threshold: Option<f32>, unclip_ratio: Option<f32>) -> Result<(), Error> {
+
+
+        Ok(())
     }
 
     fn resize(&self, img: &Mat, limit_side_len: i32) -> Result<(Mat, Array1<f32>), Error> {
@@ -65,8 +93,8 @@ impl TextDetector {
     }
 
     fn normalize(&self, img: &Mat, mean: Vec<f32>, std: Vec<f32>, scale: f32) -> Result<Array3<f32>, Error>{
-        let mean = Array3::from_shape_vec((1, 1, 3), mean).unwrap();
-        let std = Array3::from_shape_vec((1, 1, 3), std).unwrap();
+        // let mean = Array3::from_shape_vec((1, 1, 3), mean).unwrap();
+        // let std = Array3::from_shape_vec((1, 1, 3), std).unwrap();
 
         println!("mean: {:?}", mean);
         println!("std: {:?}", std);
@@ -105,7 +133,7 @@ mod tests {
     fn test_resize() {
         let im_bytes: &[u8] = include_bytes!("../../../test_data/car.jpg");
         let image = convert_image_to_mat(im_bytes).unwrap();
-        let detector = TextDetector::new();
+        let detector = TextDetector::new("", None, None).unwrap();
         detector.resize(&image, 960).unwrap();
     }
 
@@ -113,10 +141,8 @@ mod tests {
     fn test_normalize() {
         let im_bytes: &[u8] = include_bytes!("../../../test_data/car.jpg");
         let image = convert_image_to_mat(im_bytes).unwrap();
-        let detector = TextDetector::new();
+        let detector = TextDetector::new("../../../weights/ppocrv4/ch_PP-OCRv4_det_infer.onnx", None, None).unwrap();
         detector.normalize(&image, vec![0.485, 0.456, 0.406], vec![0.229, 0.224, 0.225], 1f32/255f32);
-
-
     }
 
 }
